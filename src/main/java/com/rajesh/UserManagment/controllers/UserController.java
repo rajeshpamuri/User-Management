4238f8f2f54721c0b81a8642583a1bed80283d4a
Rajesh <rajeshpamuri@gmail.com>
2024-09-09 09:40:50 +0530

    Third Commit

--- src/main/java/com/rajesh/UserManagment/controllers/UserController.java
+++ src/main/java/com/rajesh/UserManagment/controllers/UserController.java
@@ -5,7 +5,7 @@ import com.rajesh.UserManagment.Dtos.*;
 import com.rajesh.UserManagment.Entitys.User;
 import com.rajesh.UserManagment.repositories.UserRepo;
 import com.rajesh.UserManagment.services.DashService;
-import com.rajesh.UserManagment.services.EmailService;
+
 import com.rajesh.UserManagment.services.UserService;
 
 import jakarta.mail.MessagingException;
@@ -18,103 +18,125 @@ import java.util.Map;
 
 @Controller
 public class UserController {
-    
-   @Autowired
-   private DashService dashService;
 
-   @Autowired
-   private UserRepo userRepo;
+    @Autowired
+    private DashService dashService;
+
+    @Autowired
+    private UserRepo userRepo;
 
     @Autowired
     private UserService userService;
 
     @GetMapping("/registerForm")
-    public String loadRegiserPage(Model model){
-        RegisterFormDTO registerFormDTO=new RegisterFormDTO();
+    public String loadRegiserPage(Model model) {
+        RegisterFormDTO registerFormDTO = new RegisterFormDTO();
         Map<Integer, String> countries = userService.getCountries();
-        model.addAttribute("countries",countries);
-        model.addAttribute("registerForm",registerFormDTO);
-        return"register";
+        model.addAttribute("countries", countries);
+        model.addAttribute("registerForm", registerFormDTO);
+        return "register";
     }
 
     @GetMapping("/states/{countryId}")
     @ResponseBody
-    public Map<Integer, String> getStates(@PathVariable Integer countryId){
+    public Map<Integer, String> getStates(@PathVariable Integer countryId) {
         Map<Integer, String> states = userService.getStates(countryId);
         return states;
     }
+
     @GetMapping("/cities/{stateId}")
     @ResponseBody
-    public Map<Integer, String> getCities(@PathVariable Integer stateId){
+    public Map<Integer, String> getCities(@PathVariable Integer stateId) {
         Map<Integer, String> cities = userService.getCities(stateId);
         return cities;
     }
 
     @PostMapping("/register")
-    public String handelForm(@ModelAttribute("registerForm") RegisterFormDTO registerFormDTO,Model model) throws MessagingException {
+    public String handelForm(@ModelAttribute("registerForm") RegisterFormDTO registerFormDTO, Model model) throws MessagingException {
         boolean status = userService.duplicateEmailCheck(registerFormDTO.getEmail());
-        if (status){
-            model.addAttribute("emsg","Duplicate Email Found");
+        if (status) {
+            model.addAttribute("emsg", "Duplicate Email Found");
 
-        }else {
+        } else {
             boolean saveUser = userService.saveUser(registerFormDTO);
-            if (saveUser){
-                model.addAttribute("smsg","Register Successes And Check Email to get Password");
+            if (saveUser) {
+                model.addAttribute("smsg", "Register Successes And Check Email to get Password");
 
-            }else{
-                model.addAttribute("emsg","Registration Failed ");
+            } else {
+                model.addAttribute("emsg", "Registration Failed ");
             }
 
         }
         Map<Integer, String> countries = userService.getCountries();
-        model.addAttribute("countries",countries);
+        model.addAttribute("countries", countries);
         return "register";
 
     }
 
-   @GetMapping("/")
+    @GetMapping("/")
     public String getLoginForm(Model model) throws MessagingException {
-       LoginFormDTO loginFormDTO=new LoginFormDTO();
-        model.addAttribute("loginForm",loginFormDTO);
+        LoginFormDTO loginFormDTO = new LoginFormDTO();
+        model.addAttribute("loginForm", loginFormDTO);
         return "login";
     }
 
     @PostMapping("/login")
-    public String loginValidity(@ModelAttribute("loginForm") LoginFormDTO loginFormDTO, Model model){
+    public String loginValidity(@ModelAttribute("loginForm") LoginFormDTO loginFormDTO, Model model) {
         User byEmail = userRepo.findByEmail(loginFormDTO.getEmail());
         UserDTO login = userService.login(loginFormDTO);
-        if (login==null) {
-         model.addAttribute("emsg", "Invalid credential");
-        }else {
-            if ("Yes".equals(byEmail.getPassRest())){
-                return"redirect:dashboard";
-            }else {
-                return"redirect:resetPwd?email="+login.getEmail();
+        if (login == null) {
+            model.addAttribute("emsg", "Invalid credential");
+        } else {
+            if ("Yes".equals(byEmail.getPassRest())) {
+                return "redirect:dashboard";
+            } else {
+                return "redirect:resetpwd?email=" + login.getEmail();
             }
         }
         return "login";
     }
+
     @GetMapping("/dashboard")
-    public  String dashBoar(Model model){
+    public String dashBoar(Model model) {
         QuoteApiResponseDTO quote = dashService.getQuote();
-        model.addAttribute("quote",quote);
-        return"dashboard";
+        model.addAttribute("quote", quote);
+        return "dashboard";
     }
-    @GetMapping("/resetPwd")
-    public String restPwd(@RequestParam String email, Model model){
-        ResetPwdFormDTO resetPwdFormDTO=new ResetPwdFormDTO();
-        model.addAttribute("resetPwdForm",resetPwdFormDTO);
+
+    @GetMapping("/resetpwd")
+    public String restPwd(@RequestParam String email, Model model) {
+        ResetPwdFormDTO resetPwdFormDTO = new ResetPwdFormDTO();
+        model.addAttribute("resetPwdForm", resetPwdFormDTO);
         resetPwdFormDTO.setEmail(email);
-        return"resetPwd";
+        return "resetPwd";
     }
 
     @PostMapping("/resetPage")
-    public String handelRestPed(@ModelAttribute("resetPwdForm") ResetPwdFormDTO resetPwdFormDTO,Model model){
-        boolean b = userService.resetPwd(resetPwdFormDTO);
-        if (b) {
-            return "redirect:dashboard";
-        }
-        return"resetPwd";
-    }
+    public String handelRestPed(@ModelAttribute("resetPwdForm") ResetPwdFormDTO resetPwdFormDTO, Model model) {
+        User byEmail = userRepo.findByEmail(resetPwdFormDTO.getEmail());
+        //Without Validating New Password And Confirm Password
+//        boolean b = userService.resetPwd(resetPwdFormDTO);
+//
+//        if (b) {
+//            return "redirect:dashboard";
+//        }
+//        return"resetPwd";
+        //below code Validate The New Password And Confirm Password
 
+        if (resetPwdFormDTO.getEmail().equals(byEmail.getEmail()) && resetPwdFormDTO.getNewPwd().equals(resetPwdFormDTO.getConPwd())) {
+            // model.addAttribute("emsg", "Email Or Password Didn't Match");
+            boolean b = userService.resetPwd(resetPwdFormDTO);
+            return "redirect:dashboard";
+        } else {
+            // boolean b = userService.resetPwd(resetPwdFormDTO);
+            model.addAttribute("emsg", "Email Or Password Didn't Match");
+            return"resetpwd";
+        }
     }
+}
+
+
+
+
+
+
